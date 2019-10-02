179ea948ed4f4ff222bfd628adb91a4381d206a6
BAHROUN Rayan <rayan.bahroun@IGTP5-PC10.isim.intra>
2019-10-02 11:17:02 +0200

    Suite simplechat 2

---
+++ SimpleChat2/src/ServerConsole.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import common.ChatIF;
+
+public class ServerConsole implements ChatIF{
+	  EchoServer server;
+
+	  public ServerConsole(int port) 
+	  {
+		  server = new EchoServer(port);
+	  }
+
+	  
+	  //Instance methods ************************************************
+	  
+	  /**
+	   * This method waits for input from the console.  Once it is 
+	   * received, it sends it to the server message handler.
+	   */
+	  public void accept() 
+	  {
+	    try
+	    {
+	      BufferedReader fromConsole = 
+	        new BufferedReader(new InputStreamReader(System.in));
+	      String message;
+
+	      while (true) 
+	      {
+	        message = fromConsole.readLine();
+	        server.handleMessageFromServerConsole(message);
+	      }
+	    } 
+	    catch (Exception ex) 
+	    {
+	      System.out.println
+	        ("Unexpected error while reading from console!");
+	    }
+	  }
+
+	  /**
+	   * This method overrides the method in the ChatIF interface.  It
+	   * displays a message onto the screen.
+	   *
+	   * @param message The string to be displayed.
+	   */
+	  public void display(String message) 
+	  {
+	    System.out.println("Server MSG> " + message);
+	  }
+
+	  
+	  //Class methods ***************************************************
+	  
+	  /**
+	   * This method is responsible for the creation of the Client UI.
+	   *
+	   * @param args[0] The host to connect to.
+	   */
+	  public static void main(String[] args) 
+	  {
+	    String host = "";
+	    int port = 0;  //The port number
+
+	    try
+	    {
+	      host = args[0];
+	    }
+	    catch(ArrayIndexOutOfBoundsException e)
+	    {
+	      host = "localhost";
+	    }
+	    ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
+	    chat.accept();  //Wait for console data
+	  }
+}
